85848ca1959b87e9e7d249a209adf56827950917
Adam Grob <ad4mgr0b@gmail.com>
2018-05-30 10:32:25 +0200

    Created a basic doget bemthod, which is using a template.

--- src/main/java/com/codecool/labourent/controllers/ListPageController.java
+++ src/main/java/com/codecool/labourent/controllers/ListPageController.java
@@ -1,5 +1,9 @@
 package com.codecool.labourent.controllers;
 
+import com.codecool.labourent.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +15,8 @@ import java.io.IOException;
 public class ListPageController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("listPage.html", context, resp.getWriter());
     }
 }
