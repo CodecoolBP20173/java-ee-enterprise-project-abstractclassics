b374fe6c20ded73b2126cebb898fd80f8f93b39d
Peter Kondacs <kondacspeter@gmail.com>
2018-05-30 14:57:39 +0200

    Getting user data from the registration form.

--- src/main/java/com/codecool/labourent/controllers/RegistrationPageController.java
+++ src/main/java/com/codecool/labourent/controllers/RegistrationPageController.java
@@ -2,6 +2,7 @@ package com.codecool.labourent.controllers;
 
 
 import com.codecool.labourent.config.TemplateEngineUtil;
+import org.mindrot.jbcrypt.BCrypt;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -11,19 +12,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/registration"})
 public class RegistrationPageController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("registration.html", context, resp.getWriter());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("registration.html", context, response.getWriter());
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("/login");
+    protected void doPost(HttpServletRequest request, HttpServletResponse servletResponse) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = hashPassword(request.getParameter("password"));
+
+        servletResponse.sendRedirect("/login");
+    }
+
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(16));
     }
 }
