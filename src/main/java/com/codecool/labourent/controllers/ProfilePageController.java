df7d9331be1a1d2899b98ad69ee84b4932e0a2a5
Orsolya Lévai <solya0082@gmail.com>
2018-05-30 16:07:39 +0200

    Sent the details into the form if the user already filled in

--- src/main/java/com/codecool/labourent/controllers/ProfilePageController.java
+++ src/main/java/com/codecool/labourent/controllers/ProfilePageController.java
@@ -7,9 +7,7 @@ import com.codecool.labourent.model.UserDetail;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,22 +21,61 @@ import java.util.stream.Stream;
 public class ProfilePageController extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //System.err.println("I'm on the user profile page");
-        /*EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpaexamplePU");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        UserDetail userDetail = new UserDetail();*/
-        String[] genders = Stream.of(Gender.values()).map(Gender::name).toArray(String[]::new);
 
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("userId", 1);
+        String[] genders = Stream.of(Gender.values()).map(Gender::name).toArray(String[]::new);
+        int userId = 1;
+        UserDetail details = new UserDetail();
+
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("labourentPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+        try {
+            TypedQuery<UserDetail> queryUserDetail =
+                    entityManager.createQuery("SELECT c FROM UserDetail c WHERE c.id = :userId", UserDetail.class);
+            details = queryUserDetail.setParameter("userId", userId).getSingleResult();
+
+            System.err.println(queryUserDetail);
+        } catch (NoResultException e) {
+
+        }
+
+
+
+
+        //queryUserDetail.setParameter("userId", userId).getSingleResult();
+
+        /*EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(address);
+        transaction.commit();*/
+        /*TypedQuery<Country> query = em.createQuery(
+                "SELECT c FROM Country c WHERE c.name = :name", Country.class);
+        return query.setParameter("name", name).getSingleResult();*/
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+
+
         context.setVariable("genders", genders);
+        context.setVariable("userDetails", details);
 
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         engine.process("profile.html", context, resp.getWriter());
-        //entityManager.close();
-        //entityManagerFactory.close();
 
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpaexamplePU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
