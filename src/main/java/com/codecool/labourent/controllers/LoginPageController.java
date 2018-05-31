1a7860ad989e48c3c2e3a4a8663a097e9ec77c15
Peter Kondacs <kondacspeter@gmail.com>
2018-05-31 10:40:29 +0200

    Added basic login form.

--- src/main/java/com/codecool/labourent/controllers/LoginPageController.java
+++ src/main/java/com/codecool/labourent/controllers/LoginPageController.java
@@ -1,5 +1,9 @@
 package com.codecool.labourent.controllers;
 
+import com.codecool.labourent.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +14,9 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/login"})
 public class LoginPageController extends HttpServlet{
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("login.html", context, response.getWriter());
     }
 }
