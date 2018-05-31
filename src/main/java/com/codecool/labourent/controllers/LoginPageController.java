eaeb62c7e151a913fc82e5235c46ab785a69d3e5
Peter Kondacs <kondacspeter@gmail.com>
2018-05-31 13:11:45 +0200

    Login is working without session.

--- src/main/java/com/codecool/labourent/controllers/LoginPageController.java
+++ src/main/java/com/codecool/labourent/controllers/LoginPageController.java
@@ -1,6 +1,9 @@
 package com.codecool.labourent.controllers;
 
 import com.codecool.labourent.config.TemplateEngineUtil;
+import com.codecool.labourent.dbConnection.UserAccountQueries;
+import com.codecool.labourent.model.UserAccount;
+import org.mindrot.jbcrypt.BCrypt;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -18,5 +21,28 @@ public class LoginPageController extends HttpServlet{
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
         engine.process("login.html", context, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        if (passwordIsCorrect(email, password)) {
+            response.sendRedirect("/list");
+        } else {
+            response.sendRedirect("/login?invalid=true");
+        }
+    }
+
+    private boolean passwordIsCorrect(String email, String plainTextPassword) {
+        UserAccount userAccount = UserAccountQueries.getUserAccountByEmail(email);
+
+        if (userAccount == null) {
+            return false;
+        }
+
+        String hashedPassword = userAccount.getPassword();
+        return BCrypt.checkpw(plainTextPassword, hashedPassword);
     }
 }
