84c7fcfe25f6ac5bd0a944af79e746d5bd24b5fa
Orsolya Lévai <solya0082@gmail.com>
2018-05-31 09:45:05 +0200

    Added some docstring

--- src/main/java/com/codecool/labourent/dbConnection/ProfilePageQueries.java
+++ src/main/java/com/codecool/labourent/dbConnection/ProfilePageQueries.java
@@ -7,9 +7,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
-
+/**
+ * This class contains the querries which work on the userdetail relation.
+ */
 public class ProfilePageQueries {
 
+    /**
+     * It gives back a UserDetail instance, if the user has already created a profile. If not, it returns a default instance.
+     * @param userId: int
+     * @return UserDetail instance
+     */
     public static UserDetail getUserDetailById(int userId) {
         EntityManager entityManager = EntityManagerSingleton.getInstance();
         UserDetail userDetails = new UserDetail();
@@ -23,7 +30,8 @@ public class ProfilePageQueries {
         } catch (NoResultException e) {
             System.err.println("No user's details are found by the given user id!");
         }
-
         return userDetails;
     }
+
+
 }
