8a4b299668c684daeaa4778da99dc3be19eab731
Peter Kondacs <kondacspeter@gmail.com>
2018-06-13 16:14:02 +0200

    Tests for unsuccesful registration because of taken email.

--- src/tests/com/codecool/labourent/controllers/RegistrationPageControllerTest.java
+++ src/tests/com/codecool/labourent/controllers/RegistrationPageControllerTest.java
@@ -1,12 +1,18 @@
 package com.codecool.labourent.controllers;
 
 import com.codecool.labourent.dbConnection.UserAccountQueries;
+import com.codecool.labourent.model.UserAccount;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.mockito.Mockito.mock;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 
 class RegistrationPageControllerTest {
 
@@ -21,5 +27,20 @@ class RegistrationPageControllerTest {
         responseMock = mock(HttpServletResponse.class);
         userAccountQueriesMock = mock(UserAccountQueries.class);
         registrationPageController = new RegistrationPageController(userAccountQueriesMock);
+    }
+
+    @Test
+    void testUserIsRedirectedIfEmailIsTaken() throws ServletException, IOException {
+        when(userAccountQueriesMock.emailIsTaken(requestMock.getParameter("email"))).thenReturn(true);
+        registrationPageController.doPost(requestMock, responseMock);
+        verify(responseMock, times(1)).sendRedirect("/registration?taken=email");
+    }
+
+    @Test
+    void testNewUserIsNotRegisteredIfEmailIsTaken() throws ServletException, IOException {
+        when(userAccountQueriesMock.emailIsTaken(requestMock.getParameter("email"))).thenReturn(true);
+        ArgumentCaptor<UserAccount> captor = ArgumentCaptor.forClass(UserAccount.class);
+        registrationPageController.doPost(requestMock, responseMock);
+        verify(userAccountQueriesMock, never()).saveUserAccount(captor.capture());
     }
 }
