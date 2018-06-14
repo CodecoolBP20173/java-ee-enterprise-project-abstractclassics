7da3329e6f6b57bbecd1aa72742d25b8e6983846
Peter Kondacs <kondacspeter@gmail.com>
2018-06-14 18:52:42 +0200

    Tests for saving a service category.

--- src/tests/java/com/codecool/labourent/dbConnection/ServiceCategoryQueriesSaveServiceCategoryTest.java
+++ src/tests/java/com/codecool/labourent/dbConnection/ServiceCategoryQueriesSaveServiceCategoryTest.java
@@ -2,10 +2,13 @@ package com.codecool.labourent.dbConnection;
 
 import com.codecool.labourent.model.ServiceCategory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,7 +25,23 @@ class ServiceCategoryQueriesSaveServiceCategoryTest {
         entityManager = Persistence.createEntityManagerFactory("labourentPUTest").createEntityManager();
         serviceCategoryQueries = new ServiceCategoryQueries(entityManager);
         serviceCategoryToSave1 = new ServiceCategory("SC", "This is a service category.");
-        serviceCategoryToSave1 = new ServiceCategory("SC2", "This is another service category.");
+        serviceCategoryToSave2 = new ServiceCategory("SC2", "This is another service category.");
         getServiceCategoriesQuery = "SELECT s FROM ServiceCategory s";
+
+        serviceCategoryQueries.saveServiceCategory(serviceCategoryToSave1);
+        serviceCategoryQueries.saveServiceCategory(serviceCategoryToSave2);
+    }
+
+    @Test
+    void testServiceCategoriesAreSaved() {
+        List<ServiceCategory> serviceCategories = entityManager.createQuery(getServiceCategoriesQuery).getResultList();
+        assertEquals(2, serviceCategories.size());
+    }
+
+    @Test
+    void testServiceCategoryIsSavedWithCorrectData() {
+        ServiceCategory serviceCategoryFromDB = entityManager.find(ServiceCategory.class, 2);
+        assertEquals("SC2", serviceCategoryFromDB.getName());
+        assertEquals("This is another service category.", serviceCategoryFromDB.getDescription());
     }
 }
