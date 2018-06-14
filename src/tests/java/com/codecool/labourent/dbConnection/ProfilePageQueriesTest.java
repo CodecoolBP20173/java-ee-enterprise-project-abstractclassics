78dc93b38ee2c2fc8dd66d10cf7c9f7676daa34c
Orsolya Lévai <solya0082@gmail.com>
2018-06-14 19:33:29 +0200

    Convert the tests to working on h2 database

--- src/tests/java/com/codecool/labourent/dbConnection/ProfilePageQueriesTest.java
+++ src/tests/java/com/codecool/labourent/dbConnection/ProfilePageQueriesTest.java
@@ -1,46 +1,64 @@
 package com.codecool.labourent.dbConnection;
 
+import com.codecool.labourent.controllers.ProfilePageController;
+import com.codecool.labourent.model.Gender;
+import com.codecool.labourent.model.UserAccount;
 import com.codecool.labourent.model.UserDetail;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.mockito.ArgumentMatchers.booleanThat;
 import static org.mockito.Mockito.mock;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProfilePageQueriesTest {
-    private EntityManager entityManagerMock;
+    private EntityManager entityManager;
     private ProfilePageQueries profilePageQueries;
-    private int testUserId;
-    private UserDetail userDetailMock;
+    private UserDetail userDetail1;
+    private UserDetail userDetail2;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
-        entityManagerMock = mock(EntityManager.class);
-        profilePageQueries = new ProfilePageQueries(entityManagerMock);
-        userDetailMock = mock(UserDetail.class);
-        testUserId = 1;
+        entityManager = Persistence.createEntityManagerFactory("labourentPUTest").createEntityManager();
+        profilePageQueries = new ProfilePageQueries(entityManager);
+        Date dateOfBirth = ProfilePageController.getFormatDate("2021-01-01");
+        UserAccount userAccount = new UserAccount("peach", "asd@asd.hu", "pw");
+        userDetail1 = new UserDetail("Apple", "Peach", "1456624", "Budapest",
+                dateOfBirth, Gender.valueOf("FEMALE"), "hello", userAccount);
+        userDetail1.setImgUrl("/");
+
+        UserAccount userAccount2 = new UserAccount("lime", "limed@asd.hu", "pw");
+        userDetail2 = new UserDetail("Grape", "Lime", "456789", "Cegléd",
+                dateOfBirth, Gender.valueOf("MALE"), "hi", userAccount2);
+        userDetail2.setImgUrl("/");
+        profilePageQueries.putUserAccountInDb(userDetail2);
     }
 
     @Test
     void testGetUserById() {
         int otherUserId = 1;
-        when(entityManagerMock.find(UserDetail.class, otherUserId)).thenReturn(userDetailMock);
-        UserDetail resultUserDetail = profilePageQueries.getUserDetailById(testUserId);
-        assertEquals(userDetailMock, resultUserDetail);
+        UserDetail resultUserDetail = profilePageQueries.getUserDetailById(otherUserId);
+        assertEquals(userDetail1, resultUserDetail);
     }
 
     @Test
     void testGetUserByIdException() {
         int otherUserId = 2;
-        when(entityManagerMock.find(UserDetail.class, otherUserId)).thenReturn(userDetailMock);
         assertThrows(NoResultException.class, ()->{
-            profilePageQueries.getUserDetailById(testUserId); });
+            profilePageQueries.getUserDetailById(otherUserId); });
     }
 
+
+
+    @AfterAll
+    void tearDown() {
+        entityManager.clear();
+        entityManager.close();
+    }
 }
