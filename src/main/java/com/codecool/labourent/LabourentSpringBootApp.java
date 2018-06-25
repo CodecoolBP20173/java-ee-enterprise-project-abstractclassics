d91ff620992db8ab7786de94a6c2584717553399
Orsolya Lévai <solya0082@gmail.com>
2018-06-25 13:34:35 +0200

    Added LabourentSpringBootApp class

---
+++ src/main/java/com/codecool/labourent/LabourentSpringBootApp.java
@@ -0,0 +1,11 @@
+package com.codecool.labourent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabourentSpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LabourentSpringBootApp.class, args);
+    }
+}
