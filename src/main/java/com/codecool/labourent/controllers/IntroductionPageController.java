40b201427c9ca51e177066fd604b84815745a8cd
Peter Kondacs <kondacspeter@gmail.com>
2018-06-25 16:07:10 +0200

    Removed unnessecary imports.

--- src/main/java/com/codecool/labourent/controllers/IntroductionPageController.java
+++ src/main/java/com/codecool/labourent/controllers/IntroductionPageController.java
@@ -1,18 +1,8 @@
 package com.codecool.labourent.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Controller
