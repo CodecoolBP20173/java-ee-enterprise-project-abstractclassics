294c688690bcb48d41bd8c7a968b32b8c2f92d0e
Adam Grob <ad4mgr0b@gmail.com>
2018-06-25 02:49:20 +0200

    The list page is now fully responsive, redesigned the navbar for being full responsive across all pages.

--- src/main/webapp/static/script/navbar.js
+++ src/main/webapp/static/script/navbar.js
@@ -2,14 +2,18 @@ $(document).ready(function () {
     console.log("fuck you");
     var myURL=window.location.pathname;
     var navbuttons = document.getElementsByTagName('a');
-    console.log(myURL);
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
 
+    console.log(myURL);
     for (var i = 0; i < navbuttons.length; i++) {
         var navButton = navbuttons[i];
         var navButtonUrl = navButton.getAttribute('href');
         console.log(navButtonUrl);
         if (myURL === navButtonUrl) {
-            navButton.classList.add("currentUrl");
+            navButton.parentNode.classList.add("currentUrl");
         }
+
     }
 });
