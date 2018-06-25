e9323ff1cd1504add3a9e466c49b254b2719deca
Peter Kondacs <kondacspeter@gmail.com>
2018-06-25 16:21:52 +0200

    Handling get request for index page.

--- src/main/java/com/codecool/labourent/controllers/IndexPageController.java
+++ src/main/java/com/codecool/labourent/controllers/IndexPageController.java
@@ -1,6 +1,9 @@
 package com.codecool.labourent.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +15,9 @@ import java.io.IOException;
 @Controller
 public class IndexPageController {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("/introduction");
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView method() {
+        return new ModelAndView("redirect:" + "/introduction");
+
     }
 }
