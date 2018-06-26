d17a7fee844d5ee2ac1a4bed340ab96de0582109
Adam Grob <ad4mgr0b@gmail.com>
2018-06-26 14:33:29 +0200

    Refactored for Spring JPA usage.

--- src/main/java/com/codecool/labourent/service/ServiceCategoryService.java
+++ src/main/java/com/codecool/labourent/service/ServiceCategoryService.java
@@ -4,10 +4,6 @@ import com.codecool.labourent.model.ServiceCategory;
 import com.codecool.labourent.repository.ServiceCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Query;
 import java.util.List;
 
 @Service
@@ -16,25 +12,16 @@ public class ServiceCategoryService {
     @Autowired
     private ServiceCategoryRepository serviceCategoryRepository;
 
-    public ServiceCategoryService(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
 
     public void saveServiceCategory(ServiceCategory serviceCategory) {
-        EntityTransaction transaction = entityManager.getTransaction();
-
-        transaction.begin();
-        entityManager.persist(serviceCategory);
-        transaction.commit();
+        serviceCategoryRepository.save(serviceCategory);
     }
 
     public List<ServiceCategory> getServiceCategories() {
-        String queryString = "SELECT s FROM ServiceCategory s";
-        Query serviceCategoryQuery = entityManager.createQuery(queryString);
-        return serviceCategoryQuery.getResultList();
+        return serviceCategoryRepository.findAll();
     }
 
     public ServiceCategory getServiceCategoryById(int id) {
-        return entityManager.find(ServiceCategory.class, id);
+        return serviceCategoryRepository.findOne(id);
     }
 }
