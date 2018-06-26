ccc17f86344065ab802b1993b599ec3465fa7436
Adam Grob <ad4mgr0b@gmail.com>
2018-06-26 14:59:18 +0200

    Uncommented the introduction page.

--- src/main/java/com/codecool/labourent/controllers/IntroductionPageController.java
+++ src/main/java/com/codecool/labourent/controllers/IntroductionPageController.java
@@ -1,4 +1,3 @@
-/*
 package com.codecool.labourent.controllers;
 
 import org.springframework.stereotype.Controller;
@@ -14,4 +13,3 @@ public class IntroductionPageController {
         return "introduction";
     }
 }
-*/
