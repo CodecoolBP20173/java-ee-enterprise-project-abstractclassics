69c2507cc7af6050eeab3314e0a7b85625e4119d
Orsolya Lévai <solya0082@gmail.com>
2018-06-26 15:43:11 +0200

    Cleaned the code

--- src/main/java/com/codecool/labourent/controllers/AddServicePageController.java
+++ src/main/java/com/codecool/labourent/controllers/AddServicePageController.java
@@ -10,11 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -36,15 +32,6 @@ public class AddServicePageController {
         model.addAttribute("serviceCategories", serviceCategories);
         return "addService";
     }
-    /*@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-        WebContext context = new WebContext(request, response, request.getServletContext());
-
-        List<ServiceCategory> serviceCategories = serviceCategoryService.getServiceCategories();
-        context.setVariable("serviceCategories", serviceCategories);
-        engine.process("addService.html", context, response.getWriter());
-    }*/
 
     @RequestMapping(value = "/add-service", method = RequestMethod.POST)
     public String servicePagePostView(@RequestParam("name") String name, @RequestParam("description") String description,
@@ -63,24 +50,4 @@ public class AddServicePageController {
 
         return "redirect:profile";
     }
-    /*@Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        String serviceName = request.getParameter("name");
-        String description = request.getParameter("description");
-        double price = Integer.valueOf(request.getParameter("price"));
-        int userId = (Integer) session.getAttribute("userId");
-        int serviceCategoryId = Integer.valueOf(request.getParameter("serviceCategoryId"));
-
-        UserAccount userAccount = userAccountService.getUserAccountById(userId);
-        ServiceCategory serviceCategory = serviceCategoryService.getServiceCategoryById(serviceCategoryId);
-
-        response.sendRedirect("/profile");
-
-        Service service = new Service(serviceName, description, price);
-        service.setUserAccount(userAccount);
-        service.setServiceCategory(serviceCategory);
-        serviceService.saveService(service);
-    }*/
 }
