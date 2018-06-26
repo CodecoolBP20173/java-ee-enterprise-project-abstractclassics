9cd7161f293b9a062f81954768424128d5bc79ac
Orsolya Lévai <solya0082@gmail.com>
2018-06-26 15:12:36 +0200

    Added AddService get request

--- src/main/java/com/codecool/labourent/controllers/AddServicePageController.java
+++ src/main/java/com/codecool/labourent/controllers/AddServicePageController.java
@@ -1,7 +1,5 @@
-/*
 package com.codecool.labourent.controllers;
 
-import com.codecool.labourent.config.TemplateEngineUtil;
 import com.codecool.labourent.service.ServiceCategoryService;
 import com.codecool.labourent.service.ServiceService;
 import com.codecool.labourent.service.UserAccountService;
@@ -10,14 +8,12 @@ import com.codecool.labourent.model.ServiceCategory;
 import com.codecool.labourent.model.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -34,8 +30,13 @@ public class AddServicePageController {
     private UserAccountService userAccountService;
 
 
-
-    @Override
+    @RequestMapping(value = "/add-service", method = RequestMethod.GET)
+    public String profilePageView(Model model) {
+        List<ServiceCategory> serviceCategories = serviceCategoryService.getServiceCategories();
+        model.addAttribute("serviceCategories", serviceCategories);
+        return "addService";
+    }
+    /*@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
@@ -43,9 +44,9 @@ public class AddServicePageController {
         List<ServiceCategory> serviceCategories = serviceCategoryService.getServiceCategories();
         context.setVariable("serviceCategories", serviceCategories);
         engine.process("addService.html", context, response.getWriter());
-    }
+    }*/
 
-    @Override
+    /*@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
 
@@ -64,6 +65,5 @@ public class AddServicePageController {
         service.setUserAccount(userAccount);
         service.setServiceCategory(serviceCategory);
         serviceService.saveService(service);
-    }
+    }*/
 }
-*/
