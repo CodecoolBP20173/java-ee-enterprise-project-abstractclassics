156f7ecdbca88b973066d5b26899ee42c989d608
Peter Kondacs <kondacspeter@gmail.com>
2018-06-27 14:17:00 +0200

    Custom authentication success handler for spring security.

---
+++ src/main/java/com/codecool/labourent/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.codecool.labourent.config;
+
+import com.codecool.labourent.model.UserAccount;
+import com.codecool.labourent.service.UserAccountService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.Principal;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    UserAccountService userAccountService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String email = authentication.getName();
+        UserAccount userAccount = userAccountService.getUserAccountByEmail(email);
+        HttpSession session = request.getSession();
+        session.setAttribute("email", email);
+        session.setAttribute("userId", userAccount.getId());
+        response.sendRedirect("/list");
+    }
+}
