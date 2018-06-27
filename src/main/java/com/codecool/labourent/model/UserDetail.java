37098997f1fb5afd7bfd219c2d72e120bd30baf9
Peter Kondacs <kondacspeter@gmail.com>
2018-06-27 15:51:41 +0200

    Modified path for default profile picture.

--- src/main/java/com/codecool/labourent/model/UserDetail.java
+++ src/main/java/com/codecool/labourent/model/UserDetail.java
@@ -41,7 +41,7 @@ public class UserDetail {
         this.lastName = "";
         this.gender = Gender.UNDEFINED;
         this.introductionText = "";
-        this.imgUrl = "/static/img/default_profile.png";
+        this.imgUrl = "img/default_profile.png";
         this.dateOfBirth = Calendar.getInstance().getTime();
         this.city = "";
         this.phoneNumber = "";
@@ -53,7 +53,7 @@ public class UserDetail {
         this.lastName = "";
         this.gender = Gender.UNDEFINED;
         this.introductionText = "";
-        this.imgUrl = "/static/img/default_profile.png";
+        this.imgUrl = "img/default_profile.png";
         this.dateOfBirth = Calendar.getInstance().getTime();
         this.city = "";
         this.phoneNumber = "";
@@ -67,7 +67,7 @@ public class UserDetail {
         this.lastName = lastName;
         this.gender = gender;
         this.introductionText = introductionText;
-        this.imgUrl = "/static/img/default_profile.png";
+        this.imgUrl = "img/default_profile.png";
         this.dateOfBirth = dateOfBirth;
         this.city = city;
         this.phoneNumber = phoneNumber;
