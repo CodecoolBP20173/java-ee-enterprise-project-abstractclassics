9d4122b885fa65246aeaec5b5aaa3999e5af0dff
Adam Grob <ad4mgr0b@gmail.com>
2018-06-28 00:50:54 +0200

    The Restcontroller for the Rating logic is implemented.

--- src/main/java/com/codecool/labourent/controllers/ListPageRestController.java
+++ src/main/java/com/codecool/labourent/controllers/ListPageRestController.java
@@ -4,10 +4,14 @@ import com.codecool.labourent.model.Rating;
 import com.codecool.labourent.service.RatingService;
 import com.codecool.labourent.service.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @RestController
@@ -21,16 +25,15 @@ public class ListPageRestController {
 
     @RequestMapping(value = "/list/rating", method = RequestMethod.POST)
     public String getRating(@RequestParam("rating") Integer rating,
-                            @RequestParam("serviceId") Integer serviceId){
-
-        Integer userId = 3;
+                            @RequestParam("serviceId") Integer serviceId,
+                            HttpServletRequest request){
+        HttpSession session = request.getSession();
+        Integer userId = (Integer) session.getAttribute("userId");
 
         if (!ratingService.isRatingRelationPresent(userId, serviceId)) {
             ratingService.saveRatingRelation(userId, serviceId);
-           //Todo addTo service the stuff
+            serviceService.increaseRatingStats(rating, serviceId);
         }
-            System.out.println(serviceId);
-
         return "success";
     }
 }
