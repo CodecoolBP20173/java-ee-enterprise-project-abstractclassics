c071f82ea2d40184cb57f26420a1145aa0285d9a
Adam Grob <ad4mgr0b@gmail.com>
2018-06-27 15:42:47 +0200

    Added Rating logic related methods, ajax calls and event listeners.

--- src/main/resources/static/script/servicePage.js
+++ src/main/resources/static/script/servicePage.js
@@ -36,4 +36,65 @@ $(document).ready(function() {
             prismScene.classList.remove('scene_turn_left');
         })
     }
+
+    let ratingDivs = document.getElementsByClassName("item item-4");
+    for (let i = 0; i < ratingDivs.length; i++) {
+        let ratingDiv = ratingDivs[i];
+        let rating = ratingDiv.getAttribute("data-avgRating");
+        let roundedRating = Math.round(rating);
+
+        let stars= ratingDiv.getElementsByClassName("fa fa-star");
+        console.log(rating);
+        for( let i = 0; i < roundedRating; i++) {
+            stars[i].classList.add("checked");
+        }
+    }
+
+    let rateContainers = document.getElementsByClassName("rate_container");
+    for (let i = 0; i < rateContainers.length; i++) {
+        let stars= rateContainers[i].getElementsByClassName("fa fa-star");
+        for(let j = 0; j < stars.length; j++) {
+            let star = stars[j];
+            star.addEventListener('mouseenter', function (evt) {
+                for (let k = 0; k <= j; k++ ) {
+                    stars[k].classList.add("checked")
+                }
+            });
+            star.addEventListener('mouseleave', function (evt) {
+                for (let k = 0; k <= j; k++ ) {
+                    stars[k].classList.remove("checked")
+                }
+            });
+            star.addEventListener('click', function (evt) {
+                let rating = 0;
+                for (let k = 0; k <= j; k++ ) {
+                    stars[k].classList.add("final_checked");
+                    rating++;
+                }
+                let parentDiv = star.parentNode;
+                parentDiv.classList.add('disabled');
+                let serviceId = parentDiv.getAttribute('data-serviceId');
+                console.log(rating);
+                jQuery.ajax({
+                    url: 'list/rating',
+                    method: 'POST',
+                    data: {
+                        rating : rating,
+                        serviceId : serviceId
+                        //Todo send user UserId
+                    },
+                    success: function (data) {
+                        alert(data);
+                    },
+                    error: function(xhr, desc, err) {
+                        console.log(xhr);
+                        console.log("Details0: " + desc + "\nError:" + err);
+                    },
+                });
+            });
+
+
+        }
+    }
+
 });
