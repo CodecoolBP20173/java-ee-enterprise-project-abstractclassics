fc23361a3d1b9200df4443274b50b024e9ca6436
Peter Kondacs <kondacspeter@gmail.com>
2018-06-27 14:17:58 +0200

    Configuration of spring security for user login handling.

---
+++ src/main/java/com/codecool/labourent/config/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.codecool.labourent.config;
+
+import com.codecool.labourent.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptEncoder;
+
+    @Autowired
+    UserAccountService userAccountService;
+
+    @Autowired
+    CustomAuthenticationSuccessHandler successHandler;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/introduction", "/login", "/registration", "/list").permitAll()
+                .antMatchers("/css/**", "/script/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .successHandler(successHandler)
+                .failureUrl("/login?invalid=true")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                .logout().invalidateHttpSession(true).logoutSuccessUrl("/list")
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // userDetailsService should be changed to your user details service
+        // password encoder being the bean defined in grails-app/conf/spring/resources.groovy
+        auth.userDetailsService(userAccountService)
+                .passwordEncoder(bCryptEncoder);
+    }
+}
