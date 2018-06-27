28a561cdcbe89e863779aff2931e9c42edfa4169
Adam Grob <ad4mgr0b@gmail.com>
2018-06-27 15:44:33 +0200

    Modified the existing Repository calls from using id to using rating.

--- src/main/java/com/codecool/labourent/service/ServiceService.java
+++ src/main/java/com/codecool/labourent/service/ServiceService.java
@@ -12,14 +12,14 @@ public class ServiceService {
     private ServiceRepository serviceRepository;
 
     public List<Service> getAllRecordsFromTable(String column, String ascOrDesc){
-        if (column.equals("id") && ascOrDesc.equalsIgnoreCase("asc") ) {
-            return serviceRepository.findAllByOrderByIdAsc();
+        if (column.equals("rating") && ascOrDesc.equalsIgnoreCase("asc") ) {
+            return serviceRepository.findAllByOrderByAverageRatingAsc();
         } else if (column.equals("name") && ascOrDesc.equalsIgnoreCase("asc")) {
             return serviceRepository.findAllByOrderByNameAsc();
         } else if (column.equals("price") && ascOrDesc.equalsIgnoreCase("asc")) {
             return serviceRepository.findAllByOrderByPriceAsc();
-        } else if (column.equals("id") && ascOrDesc.equalsIgnoreCase("desc")) {
-            return serviceRepository.findAllByOrderByIdDesc();
+        } else if (column.equals("rating") && ascOrDesc.equalsIgnoreCase("desc")) {
+            return serviceRepository.findAllByOrderByAverageRatingDesc();
         } else if (column.equals("name") && ascOrDesc.equalsIgnoreCase("desc")) {
             return serviceRepository.findAllByOrderByNameDesc();
         } else if (column.equals("price") && ascOrDesc.equalsIgnoreCase("desc")) {
@@ -29,14 +29,14 @@ public class ServiceService {
     }
 
     public List<Service> getFilteredRecordsFromTable(String column, String ascOrDesc, int servicecategoryId){
-        if (column.equals("id") && ascOrDesc.equalsIgnoreCase("asc") ) {
-            return serviceRepository.findByServiceCategoryIdOrderByIdAsc(servicecategoryId);
+        if (column.equals("rating") && ascOrDesc.equalsIgnoreCase("asc") ) {
+            return serviceRepository.findByServiceCategoryIdOrderByAverageRatingAsc(servicecategoryId);
         } else if (column.equals("name") && ascOrDesc.equalsIgnoreCase("asc")) {
             return serviceRepository.findByServiceCategoryIdOrderByNameAsc(servicecategoryId);
         } else if (column.equals("price") && ascOrDesc.equalsIgnoreCase("asc")) {
             return serviceRepository.findByServiceCategoryIdOrderByPriceAsc(servicecategoryId);
-        } else if (column.equals("id") && ascOrDesc.equalsIgnoreCase("desc")) {
-            return serviceRepository.findByServiceCategoryIdOrderByIdDesc(servicecategoryId);
+        } else if (column.equals("rating") && ascOrDesc.equalsIgnoreCase("desc")) {
+            return serviceRepository.findByServiceCategoryIdOrderByAverageRatingDesc(servicecategoryId);
         } else if (column.equals("name") && ascOrDesc.equalsIgnoreCase("desc")) {
             return serviceRepository.findByServiceCategoryIdOrderByNameDesc(servicecategoryId);
         } else if (column.equals("price") && ascOrDesc.equalsIgnoreCase("desc")) {
@@ -48,4 +48,5 @@ public class ServiceService {
     public void saveService(Service service) {
         serviceRepository.save(service);
     }
+
 }
