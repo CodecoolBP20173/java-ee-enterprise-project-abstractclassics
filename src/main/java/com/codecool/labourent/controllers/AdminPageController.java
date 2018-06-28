9afecd857caab97ca6ea51f3a5d48ad55c2d8db2
Orsolya Lévai <solya0082@gmail.com>
2018-06-28 11:29:11 +0200

    Added list to the model

--- src/main/java/com/codecool/labourent/controllers/AdminPageController.java
+++ src/main/java/com/codecool/labourent/controllers/AdminPageController.java
@@ -1,11 +1,14 @@
 package com.codecool.labourent.controllers;
 
+import com.codecool.labourent.model.ServiceCategory;
 import com.codecool.labourent.service.ServiceCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class AdminPageController {
@@ -15,7 +18,8 @@ public class AdminPageController {
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String adminPageView(Model model) {
-
+        List<ServiceCategory> serviceCategories = serviceCategoryService.getServiceCategories();
+        model.addAttribute("serviceCategories", serviceCategories);
         return "adminPage";
     }
 }
