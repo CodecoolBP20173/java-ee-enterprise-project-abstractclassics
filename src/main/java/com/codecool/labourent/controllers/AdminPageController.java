90d42bb610d15ff40e1458fe00d5854043c212ab
Orsolya Lévai <solya0082@gmail.com>
2018-06-28 10:27:26 +0200

    Added SerrviceCategoryService

--- src/main/java/com/codecool/labourent/controllers/AdminPageController.java
+++ src/main/java/com/codecool/labourent/controllers/AdminPageController.java
@@ -1,5 +1,7 @@
 package com.codecool.labourent.controllers;
 
+import com.codecool.labourent.service.ServiceCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminPageController {
+
+    @Autowired
+    ServiceCategoryService serviceCategoryService;
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String adminPageView(Model model) {
